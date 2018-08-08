343f367c9f9a410bb64bba840d1102042ed00394
miyamadoKL <105kansai500@gmail.com>
2018-08-09 01:03:50 +0900

    Add calculate timedelta

--- python/subprocess1.py
+++ python/subprocess1.py
@@ -3,6 +3,10 @@ import subprocess
 import datetime
 
 def main():
+  # Get current time
+  curr_time = datetime.datetime.now()
+
+  # Get the name of all files in the current directory with subprocess
   sp1 = subprocess.run(["ls"], stdout=subprocess.PIPE, universal_newlines=True)
   files = sp1.stdout
   print(files)
@@ -19,7 +23,16 @@ def main():
       print(file)
       cmd = "gls -l --time-style='+%s' " + file + " | tr -s ' ' | cut -d' ' -f 6"
       ps2 = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, universal_newlines=True)
-      print(ps2.stdout.strip())
+      file_ts = ps2.stdout.strip()
+      print(file_ts)
+
+      # Calculate time delta between current time and file timestamp
+      delta = curr_time - datetime.datetime.fromtimestamp(int(file_ts))
+      print(delta)
+      if delta > datetime.timedelta(minutes=5):
+        print("Created older than 5 minutes.")
+      else:
+        print("Created within 5 minutes.")
 
 if __name__ == "__main__":
   main()
